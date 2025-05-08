755c1320a9251fee0a35ed2fc6c91d672eb496da
samuelsp <samp_81@yahoo.com.br>
2025-05-08 20:07:34 -0300

    modulo 11 criacao e exibicao de produtos

--- 11_MONGODB/controllers/ProductController.js
+++ 11_MONGODB/controllers/ProductController.js
@@ -1,7 +1,25 @@
 const Product = require("../models/Product");
 
 module.exports = class ProductController {
-  static showProducts(req, res) {
-    res.render("products/all");
+  static async showProducts(req, res) {
+    const products = await Product.getProducts();
+    res.render("products/all", { products });
+  }
+
+  static createProduct(req, res) {
+    res.render("products/create");
+  }
+
+  static createProductPost(req, res) {
+    const name = req.body.name;
+    const price = req.body.price;
+    const image = req.body.image;
+    const description = req.body.description;
+
+    const product = new Product(name, price, image, description);
+
+    product.save();
+
+    res.redirect("/");
   }
 };
---
+++ 11_MONGODB/models/Product.js
@@ -0,0 +1,27 @@
+const conn = require("../db/conn");
+
+class Product {
+  constructor(name, price, image, description) {
+    this.name = name;
+    this.price = price;
+    this.image = image;
+    this.description = description;
+  }
+
+  save() {
+    const product = conn.db().collection("products").insertOne({
+      name: this.name,
+      price: this.price,
+      image: this.image,
+      description: this.description,
+    });
+    return product;
+  }
+
+  static getProducts() {
+    const products = conn.db().collection("products").find().toArray();
+    return products;
+  }
+}
+
+module.exports = Product;
